154a0b7707ffa87f9416a01cf0c250c642cab58c
Matt Stine <mstine@gopivotal.com>
2014-06-24 14:32:29 -0500

    add version controller

---
+++ src/main/java/org/example/cities/VersionController.java
@@ -0,0 +1,13 @@
+package org.example.cities;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersionController {
+
+    @RequestMapping("/version")
+    public String index() {
+        return "I am version 1!";
+    }
+}
