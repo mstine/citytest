d33e0f5c454652b746ca72a40488495c04fc2dca
Matt Stine <mstine@gopivotal.com>
2014-09-23 16:48:30 +0100

    update version to London

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings Pivotal Roadshow!";
+        return "Greetings London Roadshow!";
     }
 }
