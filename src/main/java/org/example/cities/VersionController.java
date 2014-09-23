49d742feb8f3dfc1257540e2cad54ed58f37caa7
Matt Stine <mstine@gopivotal.com>
2014-09-23 10:00:00 +0100

    update version

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings NYC Roadshow!";
+        return "Greetings Pivotal Roadshow!";
     }
 }
